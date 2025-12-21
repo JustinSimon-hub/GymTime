f172eddbe7e72ff02c8a297465d448433e3a3e04
Justin Simon <justinsimoncs@gmail.com>
2025-12-21 08:15:53 -0800

    Added Models for workouts and diets

---
+++ Models/Diet.cs
@@ -0,0 +1,13 @@
+﻿namespace GymTime.Models
+{
+    public class Diet
+    {
+        public string FoodName { get; set; }
+        public int Proteins { get; set; }
+        public int Fats { get; set; }
+        public int Carbohyradates { get; set; }
+        public int Calories  { get; set; }
+
+
+    }
+}
---
+++ Models/Workout.cs
@@ -0,0 +1,11 @@
+﻿namespace GymTime.Models
+{
+    public class Workout
+    {
+        public string WorkoutName { get; set; }
+        public int Reps { get; set; }
+        public int Sets { get; set; }
+        public int PersonalRecord { get; set; }
+        
+    }
+}
