5a41b565b9c19a088fe786dc40701d0420da5a4d
Justin Simon <justinsimoncs@gmail.com>
2026-01-08 10:00:25 -0800

    Refactored Accounts controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using GymTime.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace GymTime.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserRepository _users;
+
+        public AccountController(UserRepository users)
+        {
+            _users = users;
+        }
+
+        // GET: /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        public IActionResult Register(string email, string password)
+        {
+            _users.Register(email, password);
+            return RedirectToAction("Login");
+        }
+
+        // GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            var user = _users.Login(email, password);
+
+            if (user == null)
+            {
+                ViewBag.Error = "Invalid email or password";
+                return View();
+            }
+
+            HttpContext.Session.SetInt32("UserId", user.Id);
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: /Account/Logout
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
