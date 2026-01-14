1f4df7603fe213da2e76ff04df749d96f0aadf2f
Justin Simon <justinsimoncs@gmail.com>
2026-01-13 23:10:32 -0800

    Beginning to add authentication filtering

---
+++ Controllers/Filters/AuthorizeUserAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace GymTime.Controllers.Filters
+{
+    public class AuthorizeUserAttribute
+    {
+        //Designed to solve repetitive code inside GymController for creating authentication checks.
+
+    }
+}
