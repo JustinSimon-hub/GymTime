e4003a88ad7a575c53dadc3af08eb221846f6930
Justin Simon <justinsimoncs@gmail.com>
2026-01-14 18:41:54 -0800

    feat/refactor: Rewriting a more comprehensive testing package to detect runtime bugs and prevent edge cases before occuring

---
+++ Controllers/Tests/GymControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymTime.Controllers.Tests
+{
+    public class GymControllerTests : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
