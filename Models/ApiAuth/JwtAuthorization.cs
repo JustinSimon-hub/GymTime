484ec4945a7b08b46ce90c70046e7d87bdbdda99
Justin Simon <justinsimoncs@gmail.com>
2026-01-19 20:39:47 -0800

    feat: Adding Jwt tokenization for api gateways

---
+++ Models/ApiAuth/JwtAuthorization.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace GymTime.Models.ApiAuth
+{
+    public class JwtAuthorization
+    {
+        private readonly IConfiguration _configuration;
+        public JwtAuthorization(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //public string GenerateToken(int userId, string username)
+        //{
+        //    var securityKey = new SymmetricSecurityKey()
+        //}
+
+
+
+    }
+}
