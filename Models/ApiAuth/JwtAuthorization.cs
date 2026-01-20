ae6a221d83a0a950dcf981376e78d99e4b95618e
Justin Simon <justinsimoncs@gmail.com>
2026-01-19 20:52:34 -0800

    feat: Added Jwt services folder(Models.JwtAuth)

--- Models/ApiAuth/JwtAuthorization.cs
+++ Models/ApiAuth/JwtAuthorization.cs
@@ -2,6 +2,8 @@
 using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Formats.Cbor;
 
 
 namespace GymTime.Models.ApiAuth
@@ -14,12 +16,60 @@ namespace GymTime.Models.ApiAuth
             _configuration = configuration;
         }
 
-        //public string GenerateToken(int userId, string username)
-        //{
-        //    var securityKey = new SymmetricSecurityKey()
-        //}
+        public string GenerateToken(int userId, string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                new Claim(ClaimTypes.Name, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            var token = new JwtSecurityToken(
+                issuer : _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_configuration["Jwt:ExpiryInMinutes"])),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        //Creating signing key, signing credentials, then convert bytes into string
 
 
+        public int? ValidateToken(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+                return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!);
+
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = _configuration["Jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = _configuration["Jwt:Audience"],
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = int.Parse(jwtToken.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value);
+
+                return userId;
+            }
+            catch
+            {
+                return null;
+            }
+        }
 
     }
 }
