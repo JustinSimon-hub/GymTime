d11153abc672d4f118e2accc4baa7b3777c0a61a
Justin Simon <justinsimoncs@gmail.com>
2026-01-20 22:11:20 -0800

    feat/branch: Created new branch to test apiauthcontroller seperately from main branch

---
+++ ApiControllers/ApiAuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GymTime.Models;
+using GymTime.Models.ApiAuth;
+using Microsoft.AspNetCore.Identity.Data;
+using System.Runtime.CompilerServices;
+
+namespace GymTime.ApiControllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    [Produces("application/json")]
+    public class ApiAuthController : Controller
+    {
+        private readonly UserRepository _userRepository;
+        private readonly JwtServices _jwtService;
+        //Dep Inj.
+        public ApiAuthController(UserRepository userRepository, JwtServices jwtServices)
+        {
+            _userRepository = userRepository;
+            _jwtService = jwtServices;
+        }
+        //Login endpoint, gives user Jwt token 
+        [HttpPost("login")]
+        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            if(string.IsNullOrEmpty(request.Email))
+
+                //return to finish 
+
+            return View();
+
+        }
+            
+
+
+
+
+
+
+    }
+
+}
